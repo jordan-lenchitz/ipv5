7f247ec30c0b0fc130745408d63fa5c2a65387c6
Jordan Lenchitz <jordan.lenchitz@gmail.com>
2026-05-27 20:39:58 +0000

    Upgrade gradle plugins

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,10 @@
 plugins {
     // this is necessary to avoid the plugins to be loaded multiple times
     // in each subproject's classloader
-    kotlin("multiplatform") version "2.0.0" apply false
-    id("com.android.application") version "8.5.0" apply false
-    id("com.android.library") version "8.5.0" apply false
+    kotlin("multiplatform") version "2.0.21" apply false
+    id("com.android.application") version "8.6.1" apply false
+    id("com.android.library") version "8.6.1" apply false
 
-    id("org.jetbrains.compose") version "1.6.11" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "2.0.0" apply false
+    id("org.jetbrains.compose") version "1.7.0" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.0.21" apply false
 }
