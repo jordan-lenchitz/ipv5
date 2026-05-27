272917889ad76a59872c03f5d7ae51501bad87c4
Jordan Lenchitz <jordan.lenchitz@gmail.com>
2026-05-27 23:13:11 +0000

    Import Accessibility icon

--- composeApp/src/commonMain/kotlin/net/notipv6/ipv5/App.kt
+++ composeApp/src/commonMain/kotlin/net/notipv6/ipv5/App.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.*
+import androidx.compose.material.icons.filled.Accessibility
 import androidx.compose.material.icons.filled.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
