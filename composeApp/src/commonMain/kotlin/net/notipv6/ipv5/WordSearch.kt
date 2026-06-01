39f4916cafd39c5042654366d1b59e64c3140490
jordan-lenchitz <jordan.lenchitz@gmail.com>
2026-06-01 22:55:17 +0000

    Restore accidentally deleted state variables in WordSearch.kt

--- composeApp/src/commonMain/kotlin/net/notipv6/ipv5/WordSearch.kt
+++ composeApp/src/commonMain/kotlin/net/notipv6/ipv5/WordSearch.kt
@@ -136,6 +136,8 @@ fun WordSearchPanel() {
     }
 
     var selectedStart by remember { mutableStateOf<Pair<Int, Int>?>(null) }
+    var foundWords by remember { mutableStateOf(setOf<String>()) }
+    val isIpv7 = GlobalAppState.ipv7Mode.value
     val isAccessible = GlobalAppState.accessibilityMode.value
     val textColor = if (isAccessible) Color.Black else GlobalAppState.currentTextColor.value
     val foundColor = Color(0xFF4DB6AC) // Soul-soothing soft teal
