49dfb6cc3e0d16de866183178f842d170459ed2e
jordan-lenchitz <jordan.lenchitz@gmail.com>
2026-06-03 12:44:13 +0000

    fix errors (we hope)

--- composeApp/src/commonMain/kotlin/net/notipv6/ipv5/WordSearch.kt
+++ composeApp/src/commonMain/kotlin/net/notipv6/ipv5/WordSearch.kt
@@ -14,6 +14,7 @@ import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.onSizeChanged
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -401,10 +402,6 @@ private fun checkSelection(start: Pair<Int, Int>, end: Pair<Int, Int>, engine: W
             onFound(reverseS)
         } else {
             onFound(null)
-        }
-    }
-}
-      onFound(null)
         }
     }
 }
