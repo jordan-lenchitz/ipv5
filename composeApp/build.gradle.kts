0776293ba2435abb39ef5b04553677ae581632b9
Jordan Lenchitz <59626477+jordan-lenchitz@users.noreply.github.com>
2026-06-24 08:23:07 -0500

    chore: update versioning after myspace fix

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -66,8 +66,8 @@ android {
         applicationId = "net.notipv6.ipv5"
         minSdk = 24
         targetSdk = 35
-        versionCode = 6
-        versionName = "1.1.0"
+        versionCode = 7
+        versionName = "1.1.1"
     }
     signingConfigs {
         create("release") {
